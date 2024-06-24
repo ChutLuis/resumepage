fc5e6e0ad6b92b4d93248b8a670df5816c2e8a5a
Luis Ortiz <l.chuta2014ig@gmail.com>
2024-06-24 12:39:23 -0600

    Fixes

--- src/components/Tech.tsx
+++ src/components/Tech.tsx
@@ -5,7 +5,7 @@ import { technologies } from "../constants";
 const Tech = () => {
   return (
     <div className="flex flex-row flex-wrap justify-center gap-10">
-      {technologies.map((tech, index) => (
+      {technologies.map((tech) => (
         <div className="w-28 h-28 " key={tech.name}>
           <BallCanvas icon={tech.icon} />
         </div>
